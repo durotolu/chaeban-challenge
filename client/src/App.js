1f1ceda537ba0acafc7c3471939dfd3a59df627a
Modurotolu Olokode <durolawk@gmail.com>
2020-11-26 21:45:31 +0100

    rendered output page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Input from './components/Input'
+import Input from './components/Input';
+import Output from './components/Output';
 
 const initialinputFields = {
   date: "",
@@ -10,6 +11,7 @@ const initialinputFields = {
 
 function App() {
   const [inputFields, setInputFields] = useState(initialinputFields);
+  const [outputFields, setOutputFields] = useState({});
 
   const onValueChange = e => {
     setInputFields({
@@ -19,7 +21,7 @@ function App() {
   };
 
   const onCreateAccount = e => {
-    console.log(inputFields);
+    setOutputFields(inputFields)
     setInputFields(initialinputFields)
   }
 
@@ -27,6 +29,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Input inputFields={inputFields} onValueChange={onValueChange} onCreateAccount={onCreateAccount} />
+        <Output outputFields={outputFields} />
       </header>
     </div>
   );
---
+++ client/src/components/Output.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Output({ outputFields }) {
+  // console.log(outputFields)
+  return (
+  <div>{outputFields.message}</div>
+  )
+}
+
+export default Output
